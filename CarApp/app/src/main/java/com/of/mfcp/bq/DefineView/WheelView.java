2657a0f102db6e55bf8a152b2bb6d3135d3e3055
dingwei8897 <18395588897@163.com>
2018-12-05 15:32:47 +0800

    扶手屏应用调优

--- CarApp/app/src/main/java/com/of/mfcp/bq/DefineView/WheelView.java
+++ CarApp/app/src/main/java/com/of/mfcp/bq/DefineView/WheelView.java
@@ -181,7 +181,7 @@ public class WheelView extends ScrollView {
         TextView tv = new TextView(context);
         tv.setLayoutParams(new LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT));
         tv.setSingleLine(true);
-        tv.setTextSize(TypedValue.COMPLEX_UNIT_SP, 20);
+        tv.setTextSize(TypedValue.COMPLEX_UNIT_SP, 20);tv.setTextSize(TypedValue.COMPLEX_UNIT_SP, 20);
         tv.setText(item);
         tv.setGravity(Gravity.CENTER);
         int padding = dip2px(15);
@@ -277,9 +277,11 @@ public class WheelView extends ScrollView {
                 return;
             }
             if (position == i) {
-                itemView.setTextColor(Color.parseColor("#0288ce"));
+                itemView.setTextColor(Color.parseColor("#bff6ff"));//选中时字变色
+                itemView.setTextSize(TypedValue.COMPLEX_UNIT_SP, 35);//选中时字变大
             } else {
                 itemView.setTextColor(Color.parseColor("#bbbbbb"));
+                itemView.setTextSize(TypedValue.COMPLEX_UNIT_SP, 20);
             }
         }
     }
